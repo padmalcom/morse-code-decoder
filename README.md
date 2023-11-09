e24e68a14d71f30a1c4d8762c95d48957a229ef9
padmalcom <j.freiknecht@googlemail.com>
2023-11-09 12:22:33 +0100

    Word detection by word list.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# morse-code-decoder
+# Morse Code Decoder
+Is Morse code without spaces uniquely decipherable? Unfortunately, not. But this recursive python application calculates all possible combinations of characters including letters, numbers and punctuation.
+
+It furthermore inferes spaces between those words and filter uncommon combination of words with few characters. The spaces inference is based on a german dictionary (source https://gist.github.com/MarvinJWendt/2f4f4154b8ae218600eb091a5706b5f4). If you want to use the app for another language, replace the txt file.
+
+## Memory consumption
+The basic recusion decipher function works memory efficient, even for longer sentences it requires less than 100mb memory. In contranst, infering spaces allocates more memory due to reoccuring loop call in the main function. You have to keep in mind that even shorter words like '-.-.-.-....' = 'Käse' (cheese) could have 1024 possible space combinations according to https://www.cachesleuth.com/unmorse.html.
+
+## Credits
+Thanks to MarvinJWendt for the german word list and to Generic Human for the infer_spaces function.
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
@@ -9,20 +10,53 @@ MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
                     'R':'.-.', 'S':'...', 'T':'-',
                     'U':'..-', 'V':'...-', 'W':'.--',
                     'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    'Ä':'.-.-', 'Ü':'..--', 'Ö':'---.',
+                    'ß':'...--..',
                     '1':'.----', '2':'..---', '3':'...--',
                     '4':'....-', '5':'.....', '6':'-....',
                     '7':'--...', '8':'---..', '9':'----.',
                     '0':'-----', ', ':'--..--', '.':'.-.-.-',
                     '?':'..--..', '/':'-..-.', '-':'-....-',
                     '(':'-.--.', ')':'-.--.-'}
+           
+# wordlist source https://gist.github.com/MarvinJWendt/2f4f4154b8ae218600eb091a5706b5f4           
+words = open("wordlist-german.txt", encoding="utf8").read().upper().split()
+wordcost = dict((k, math.log((i+1)*math.log(len(words)))) for i,k in enumerate(words))
+maxword = max(len(x) for x in words)
 
-code = ".-...-"
-
-# detect three times the same char
-def consecutive(s):
+def consecutive(s: str) -> bool:
+    """
+    detect three times the same char
+    """
     return re.search(r'(.)\1\1', s)
+    
+def infer_spaces(s: str) -> list[str]:
+    """
+        Infers spaces between words from a word list based on a cost function.
+        Source https://stackoverflow.com/a/11642687/2173320
+    """
+    def best_match(i):
+        candidates = enumerate(reversed(cost[max(0, i-maxword):i]))
+        return min((c + wordcost.get(s[i-k-1:i], 9e999), k+1) for k,c in candidates)
 
-def decipher(code: str, word: str, removeConsecutive: bool = False) -> list:
+    cost = [0]
+    for i in range(1,len(s)+1):
+        c,k = best_match(i)
+        cost.append(c)
+
+    out = []
+    i = len(s)
+    while i>0:
+        c,k = best_match(i)
+        assert c == cost[i]
+        out.append(s[i-k:i])
+        i -= k    
+    return " ".join(reversed(out))
+    
+def decipher(code: str, word: str, removeConsecutive: bool = False) -> list[str]:
+    """
+        Recursively find all possible combinations of letters of a decuded morse code consisting of '-', and '.'
+    """
     result = []
     for letter, morse in MORSE_CODE_DICT.items():
         if code.startswith(morse):
@@ -36,8 +70,32 @@ def decipher(code: str, word: str, removeConsecutive: bool = False) -> list:
                 result.extend(decipher(new_code, new_word))
     return result
 
+
+def n_letter_filter(words:list[str], min_letter:int=3, min_letter_percentage:float=0.3) -> bool:
+    """
+        Returns false, if the words list contains more than min_letter_percentage percent of words with lesser than min_letter characters.
+    """
+    result = []
+    one_letter_count = 0
+    for w in words:
+        if len(w) < min_letter:
+            one_letter_count += 1
+    percentage = one_letter_count / len(words)
+    if percentage > min_letter_percentage:
+        return False
+    return True
+    
+
 if __name__ == '__main__':
+    #code = "-.-.-.-...." # Käse, Kekse
+    code = "-.-.-.-.........-.-...-.-.-.-..-." # Käse ist lecker
     results = decipher(code, "")
     print("There are", len(results), "combinations.")
     for r in results:
-        print(r)
+    
+        # infer spaces between words from word list
+        with_spaces = infer_spaces(r)
+        
+        # filter all sentences with more than 30% of words < 3 characters
+        if n_letter_filter(with_spaces.split(), 3, 0.3):
+            print("raw:", r, "with spaces:", with_spaces)
